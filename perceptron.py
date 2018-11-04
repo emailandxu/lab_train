0447e10d435134fcac9e49ab78ad42ee7d6ba5b0
emailandxu <emailandxu@qq.com>
2018-11-04 10:59:55 +0800

    add perceptron 11月4日

---
+++ perceptron.py
@@ -0,0 +1,32 @@
+import numpy as np
+import math
+
+# w1p1 + w2p2 + ...
+def add(weights,inputs):
+    assert len(weights) == len(inputs)
+    s = 0 
+    for index,weight in enumerate(weights):
+        result = weight * inputs[index]
+        s += result
+        # print(index,result)
+    return s
+
+def func_liner(input_):
+    return input_
+
+def func_sigmoid(input_):
+    return 1/(1+math.exp(-input_))
+
+def func_relu(input_):
+    if input_ < 0:
+        return 0
+    elif input_ < 1:
+        return input_
+    else:
+        return 1
+
+s = add([3,2,3],[2,4,7])
+print(s)
+print("Sigmoid",func_sigmoid(s))
+print("Relu",func_relu(s))
+
